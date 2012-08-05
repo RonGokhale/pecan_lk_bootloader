57946740ae9d0f059d395a66b0f02ce8d41bf72a
Ron <cips173@gmail.com>
2012-08-05 13:23:36 +0530

    fix

--- target/init.c
+++ target/init.c
@@ -1,247 +1,72 @@
 /*
- * Copyright (c) 2009, Google Inc.
- * All rights reserved.
- * Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2008 Travis Geiselbrecht
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *  * Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *  * Neither the name of Google, Inc. nor the names of its contributors
- *    may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
+#include <err.h>
 #include <debug.h>
-#include <dev/keys.h>
-#include <dev/gpio_keypad.h>
-#include <lib/ptable.h>
-#include <dev/flash.h>
-#include <smem.h>
+#include <target.h>
+#include <compiler.h>
 
-#define LINUX_MACHTYPE  3013
-//1007014
-
-#define VARIABLE_LENGTH        0x10101010
-#define DIFF_START_ADDR        0xF0F0F0F0
-#define NUM_PAGES_PER_BLOCK    0x40
-
-static struct ptable flash_ptable;
-
-/* for these partitions, start will be offset by either what we get from
- * smem, or from the above offset if smem is not useful. Also, we should
- * probably have smem_ptable code populate our flash_ptable.
- *
- * When smem provides us with a full partition table, we can get rid of
- * this altogether.
- *
+/* 
+ * default implementations of these routines, if the target code
+ * chooses not to implement.
  */
-static struct ptentry board_part_list[] = {
-	{
-		.start = 0,
-		.length = 40 
-		.name = "boot",
-	},
-	{
-		.start = 56,
-		.length = 512 
-		.name = "cache",
-	},
-	{
-		.start = 568,
-		.length = 40 
-		.name = "recovery",
-	},
-	{
-		.start = 608,
-		.length = 3 ,
-		.name = "splash",
-	},
-	{
-		.start = 611,
-		.length = 1560
-		.name = "system",
-	},
-	{
-		.start = 1891,
-		.length = 22 
-		.name = "lgdrm",
-	},
-	{
-		.start = 1913,
-		.length = 1788 
-		.name = "userdata",
-	},
-	{
-		.start = 3701,
-		.length = 8 ,
-		.name = "misc",
-	},
-	{
-		.start = -109,
-		.length = 4 
-		.name = "usd",
-	},
-	{
-		.start = -105,
-		.length = 45 
-		.name = "pkg",
-	},
-	{
-		.start = 0,
-		.length = 0,
-		.name = "",
-	},
-};
-static int num_parts = sizeof(board_part_list)/sizeof(struct ptentry);
 
-void smem_ptable_init(void);
-unsigned smem_get_apps_flash_start(void);
-
-void keypad_init(void);
-
-int target_is_emmc_boot(void);
-
-void target_init(void)
+__WEAK void target_early_init(void)
 {
-	unsigned offset;
-	struct flash_info *flash_info;
-	unsigned total_num_of_blocks;
-	bool  start_addr_changed = false;
-	unsigned next_ptr_start_adr = 0;
-	int i;
+}
 
-	//display_init();
-	dprintf(INFO, "target_init()\n");
+__WEAK void target_init(void)
+{
+}
 
-#if (!ENABLE_NANDWRITE)
-	keys_init();
-	keypad_init();
+__WEAK void *target_get_scratch_address(void)
+{
+    return (void *)(SCRATCH_ADDR);
+}
+
+__WEAK int target_is_emmc_boot(void)
+{
+#if _EMMC_BOOT
+    return 1;
+#else
+    return 0;
 #endif
-
-	if (target_is_emmc_boot())
-		return;
-
-	ptable_init(&flash_ptable);
-	smem_ptable_init();
-
-	flash_init();
-	flash_info = flash_get_info();
-	ASSERT(flash_info);
-
-	offset = smem_get_apps_flash_start();
-	if (offset == 0xffffffff)
-	        while(1);
-
-	total_num_of_blocks = (flash_info->block_size)/NUM_PAGES_PER_BLOCK;
-
-	//display_init();
-
-	//dprintf(INFO, "offset = 0x%08x\n", offset);
-
-	for (i = 0; i < num_parts; i++) {
-		struct ptentry *ptn = &board_part_list[i];
-		unsigned len = ptn->length;
-
-		if(len == VARIABLE_LENGTH)
-		{
-		        start_addr_changed = true;
-			unsigned length_for_prt = 0;
-			int j;
-			for (j = i+1; j < num_parts; j++)
-			{
-			        struct ptentry *temp_ptn = &board_part_list[j];
-			        ASSERT(temp_ptn->length != VARIABLE_LENGTH);
-			        length_for_prt += temp_ptn->length;
-			}
-		        len = (total_num_of_blocks - 1) - (offset + ptn->start + length_for_prt);
-			//ASSERT(len >= 0);
-		        next_ptr_start_adr = ptn->start + len;
-		}
-		if((ptn->start == DIFF_START_ADDR) && (start_addr_changed))
-		{
-		        ASSERT(next_ptr_start_adr);
-			ptn->start = next_ptr_start_adr;
-			next_ptr_start_adr = ptn->start + ptn->length;
-		}
-		ptable_add(&flash_ptable, ptn->name, offset + ptn->start,
-			   len, ptn->flags, TYPE_APPS_PARTITION, PERM_WRITEABLE);
-	}
-
-	smem_add_modem_partitions(&flash_ptable);
-
-	//display_init();
-	ptable_dump(&flash_ptable);
-	flash_set_ptable(&flash_ptable);
 }
 
-unsigned board_machtype(void)
+__WEAK unsigned check_reboot_mode(void)
 {
-    return LINUX_MACHTYPE;
-}
-
-void reboot_device(unsigned reboot_reason)
-{
-    reboot(reboot_reason);
-}
-
-unsigned check_reboot_mode(void)
-{
-    unsigned mode[2] = {0, 0};
-    unsigned int mode_len = sizeof(mode);
-    unsigned smem_status;
-
-    smem_status = smem_read_alloc_entry(SMEM_APPS_BOOT_MODE,
-					&mode, mode_len );
-    if(smem_status)
-    {
-      dprintf(CRITICAL, "ERROR: unable to read shared memory for reboot mode\n");
-      return 0;
-    }
-    return mode[0];
-}
-
-static unsigned target_check_power_on_reason(void)
-{
-    unsigned power_on_status = 0;
-    unsigned int status_len = sizeof(power_on_status);
-    unsigned smem_status;
-
-    smem_status = smem_read_alloc_entry(SMEM_POWER_ON_STATUS_INFO,
-                                        &power_on_status, status_len);
-    if (!smem_status)
-    {
-        dprintf(CRITICAL, "ERROR: unable to read shared memory for power on reason\n");
-    }
-
-    return power_on_status;
-}
-
-unsigned target_pause_for_battery_charge(void)
-{
-    if (target_check_power_on_reason() == PWR_ON_EVENT_USB_CHG)
-        return 1;
     return 0;
 }
 
-void target_battery_charging_enable(unsigned enable, unsigned disconnect)
+__WEAK void reboot_device(unsigned reboot_reason)
 {
+}
+
+__WEAK void target_battery_charging_enable
+                 (unsigned enable, unsigned disconnect)
+{
+}
+
+__WEAK unsigned target_pause_for_battery_charge(void)
+{
+    return 0;
 }
